dd73ba4f997603ecaa9fa75bd025dd208188a59b
Sabar <smtcg@umsystem.edu>
2022-10-07 13:59:14 -0500

    analysing the data with help of plotting

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Taxi-Analysis
 preform the analysis for New York taxi 
+1-plot a map of NYC and overlay a manageable number of pickup coordinates to get to a general overview of the location and distances in question. For this visualization i use the [leaflet](https://rstudio.github.io/leaflt/) package, which includes a variety of cool tools for interactive maps. in this map you can zoom and pan through the pickup locations
+
+2-investigate the variations together with disdtributions of *passenger\_count and vensdor_id by creating a multi-plot panel with different components
+
+3-
---
+++ code/NYC-Taxi.R
@@ -0,0 +1,36 @@
+library('ggplot2') # visualization
+library('scales') #visualization
+library('grid')# visualization
+library('RColorBrewer')# visualization
+library('corrplot') # visualization
+library('alluvial')# visualization
+library('dplyr')# data manipulation
+library('readr')#input/ output
+
+getwd()
+muplot <- function(... ,plotlist=NULL, file, cols=1, layout=NULL){
+  plots <- c(list(...), plotlist)
+  numPlots = length(plots)
+  if (is.null(layout)) {
+    layout <- matrix(seq(1, cols * ceiling(numPlots/cols)),
+                        ncol = cols, nrow = ceilling(numPlots/cols))
+  }
+  if(numPlots ==1) {
+    print(plots[[1]])
+  }
+  if (numPlots==1) {
+    print(plots[[1]])
+  }else {
+    grid.newpage()
+    pushViewport(viewport(layout = grid.layout(nrow(layout), ncol(layout))))
+    for (i in 1:numPlots) {
+      matchid <- as.data.frame(which(layout == i, arr.ind =TTURE))
+      print(plots[[i]], vp = viewport(layout.pos.row = matchid$row,
+                                      layout.pos.col = matchid$col))
+    }
+  }
+}
+taxi<- as_tibble(read.csv('data/Taxi.csv'))
+summary(taxi)
+glimpse(taxi)
+sum(is.na(taxi))
